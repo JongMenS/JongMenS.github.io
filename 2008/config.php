a35af42f408af0ecb9934aa6a01273e9fd4ec49b
Lode Claassen <lode@alsvanzelf.nl>
2015-09-16 09:38:26 +0200

    removing specific config useless for others
    these logins don't work anymore

--- 2008/config.php
+++ 2008/config.php
@@ -2,8 +2,8 @@
 die;
 
 [database]
-db_host = "mysql1.greenhost.nl"
-db_user = "jongereninsp1"
-db_pass = "V2ViR2VhblZp"
-db_name = "jongereninspiratiedag_nl_2008"
+db_host = ""
+db_user = ""
+db_pass = ""
+db_name = ""
 ?>
--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 die;
 
 [database]
-db_host = "mysql1.greenhost.nl"
-db_user = "jongereninsp"
-db_pass = "bW9mRWRRdWlqSQ=="
-db_name = "jongereninspiratiedag_nl"
+db_host = ""
+db_user = ""
+db_pass = ""
+db_name = ""
 ?>
